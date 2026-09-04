6e5031c2f26029cd0a5da60f11d71c04de584142
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2026-09-04 22:38:05 +0100

    Fix the weird header guard in data_version.h

--- src/game/client/components/tclient/data_version.h
+++ src/game/client/components/tclient/data_version.h
@@ -1,9 +1,7 @@
 #ifndef GAME_CLIENT_COMPONENTS_TCLIENT_DATA_VERSION_H
 #define GAME_CLIENT_COMPONENTS_TCLIENT_DATA_VERSION_H
+
 #include <base/io.h>
-#else
-#error data_version.h included multiple times
-#endif
 
 // Check validity of data/data_version.txt
 // This is extracted to this file for ease of editing
@@ -49,3 +47,5 @@ inline void CheckDataVersion(char *pError, int Length, IOHANDLE File)
 
 	io_close(File);
 }
+
+#endif
